8cb532b4e7374cdc4c3e1736ab5b0b6586ac9940
Nesia Shafira Yunindya <nesia.shafira@gmail.com>
2019-07-18 11:09:21 +0700

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
