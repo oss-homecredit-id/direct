7565ff8315cb1a3da66ef9ddcda5a60c9998816f
Nesia Shafira Yunindya <nesia.shafira@gmail.com>
2020-01-15 11:04:25 +0700

    fix component layout

--- src/components/CompLayout/CompLayout.js
+++ src/components/CompLayout/CompLayout.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./CompLayout.css";
 import Table from "../../lib/Table/Table";
+import { Button } from "../../lib/Button/Button";
 
 const CompLayout = ({
   compName,
@@ -10,7 +11,22 @@ const CompLayout = ({
   compProps,
 }) => {
   const [item, setItem] = useState(compVariation[0].var);
-  const [code, setCode] = useState(String(compVariation[0]));
+  const [code, setCode] = useState(compVariation[0].code);
+
+  const styles = {
+    typesBtn: {
+      color: "#9B9B9B",
+      padding: "5px 10px",
+      marginBottom: "0",
+    },
+    typesBtnActive: {
+      color: "#e11931",
+      padding: "5px 10px",
+      borderBottom: "2px solid #e11931",
+      borderRadius: "0",
+      marginBottom: "0",
+    },
+  };
 
   return (
     <div className="comp-container">
@@ -19,33 +35,28 @@ const CompLayout = ({
       <div className="comp-display">
         <p className="comp-text">Preview</p>
         <div className="preview">
-          <div className="comp-example">{item}</div>
           <div className="comp-variation">
-            <p className="comp-text">Types</p>
-            <div>
-              {compVariation.map((variationBtn, key) => (
-                <button
-                  key={key}
-                  className={
-                    item === variationBtn.var
-                      ? "comp-variation-btn-active"
-                      : "comp-variation-btn"
-                  }
-                  onClick={() => {
-                    console.log(variationBtn);
-                    setItem(variationBtn.var);
-                    setCode(String(variationBtn.var));
-                    console.log(item);
-                  }}
-                >
-                  {variationBtn.name}
-                </button>
-              ))}
-            </div>
+            {compVariation.map((variationBtn, key) => (
+              <Button
+                key={key}
+                variant="text"
+                onClick={() => {
+                  setItem(variationBtn.var);
+                  setCode(variationBtn.code);
+                }}
+                styleConfig={
+                  item === variationBtn.var
+                    ? styles.typesBtnActive
+                    : styles.typesBtn
+                }
+              >
+                {variationBtn.name}
+              </Button>
+            ))}
           </div>
+          <div className="comp-example">{item}</div>
         </div>
       </div>
-
       <div className="comp-code">
         <pre>{code} </pre>
       </div>
