24f133563501f9975e657cac090afacbd53ae407
Nesia Shafira Yunindya <nesia.shafira@gmail.com>
2020-01-23 16:42:33 +0700

    add home page and how to use

--- src/lib/Text/Text.js
+++ src/lib/Text/Text.js
@@ -3,21 +3,30 @@ import styled from "@emotion/styled";
 
 const H1 = styled.p`
   font-size: 30px;
+  line-height: 1.5;
 `;
 const H2 = styled.p`
   font-size: 25px;
+  line-height: 1.5;
 `;
 const H3 = styled.p`
   font-size: 20px;
+  line-height: 1.5;
 `;
 const H4 = styled.p`
   font-size: 18px;
+  line-height: 1.3;
 `;
 const H5 = styled.p`
   font-size: 16px;
+  line-height: 1.3;
 `;
 const H6 = styled.p`
   font-size: 14px;
+  line-height: 1.3;
+`;
+const P = styled.p`
+  line-height: 1.3;
 `;
 
 const Text = props => {
@@ -35,7 +44,7 @@ const Text = props => {
     case "h6":
       return <H6 style={props.styleConfig}>{props.children}</H6>;
     default:
-      return <p style={props.styleConfig}>{props.children} </p>;
+      return <P style={props.styleConfig}>{props.children}</P>;
   }
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,42 @@
 import React from "react";
-
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import { Text } from "../lib/Text/Text";
+import Illustration from "../assets/illustration.png";
+import { colors } from "../assets/mixins/mixins";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Direct" />
-  </Layout>
-);
+const IndexPage = () => {
+  const styles = {
+    container: {
+      display: "flex",
+    },
+    title: {
+      fontSize: "45px",
+      margin: "25px 0",
+      lineHeight: "1.5",
+      fontWeight: "bold",
+    },
+    image: {
+      width: "50%",
+      height: "auto",
+    },
+  };
+
+  return (
+    <Layout>
+      <SEO title="Direct" />
+      <div style={styles.container}>
+        <div>
+          <Text styleConfig={styles.title}>Direct Design System</Text>
+          <Text textType="h2">
+            Direct is Home Credit Indonesia's design system built to help
+            product teams work more efficiently
+          </Text>
+        </div>
+        <img src={Illustration} style={styles.image}></img>
+      </div>
+    </Layout>
+  );
+};
 
 export default IndexPage;
